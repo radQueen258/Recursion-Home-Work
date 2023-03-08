1324f2d924d25e5544b8a9c00de7f044dc983c61
Radka Nhapulo <radka.fnhapulo@gmail.com>
2023-03-08 13:20:09 +0200

    Exercise 4

--- src/recursive.java
+++ src/recursive.java
@@ -1,9 +1,11 @@
 import java.util.Scanner;
 
 public class recursive {
+    static int counter = 0;
     public static void main(String[] args) {
         Scanner radka = new Scanner(System.in);
-        int n ;
+        int n, number = 0;
+
 
         System.out.println("-----------------EXERCISE 1------------------");
         System.out.print("Input number: ");
@@ -15,6 +17,15 @@ public class recursive {
         System.out.println();
         System.out.println("-----------------EXERCISE 3------------------");
         System.out.println(x(n));
+        System.out.println();
+        System.out.println("-----------------EXERCISE 4------------------");
+        System.out.println("Enter a number: ");
+        number = radka.nextInt();
+        System.out.println("------NUMBER OF DIGITS------");
+        System.out.println(countDigits(number));
+        System.out.println("------SUM OF DIGITS------");
+        System.out.println(sumDigits(number));
+
 
     }
     //------------EXERCISE 1--------------
@@ -38,5 +49,17 @@ public class recursive {
     public static double x (int n) {
         if (n == 0) return 0;
         return Math.sqrt(2.0 + x (n - 1));
+    }
+    //----------EXERCISE 4--------------
+    public static int countDigits (int number) {
+        if ( number > 0) {
+            counter ++;
+            countDigits(number / 10);
+        }
+        return counter;
+    }
+    public static int sumDigits (int number) {
+        if (number == 0) return 0;
+        return (number % 10 + sumDigits(number / 10));
     }
 }
